5aa9626301a7f66d850aa5450ffba2fcb6753e25
Chris <cgregoriou9@gmail.com>
2023-08-07 20:36:09 +0300

    Implement simple coloring technique

---
+++ src/test/java/com/chgr/sudoku/solver/techniques/ChainTechniqueTest.java
@@ -0,0 +1,32 @@
+package com.chgr.sudoku.solver.techniques;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.IOException;
+import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ChainTechniqueTest extends BaseTechniqueTest{
+
+    @ParameterizedTest
+    @ValueSource(ints = {2})
+    void simpleColoring(int fileNumber) throws IOException {
+        URL url = HiddenTechniqueTest.class.getResource(String.format("/techniques/simpleColoring%d.yml", fileNumber));
+        assertNotNull(url);
+
+        BaseTechniqueTest.TechniqueEntity technique = mapper.readValue(url, BaseTechniqueTest.TechniqueEntity.class);
+        assertNotNull(technique);
+        assertNotNull(technique.grid);
+
+        loadSudoku(technique);
+
+        for(int i=0; i < technique.repetitions; i++) {
+            boolean result = ChainTechnique.simpleColoring(sudoku);
+            assertTrue(result);
+        }
+        assertChecksMatch(sudoku, technique.checks);
+    }
+}
