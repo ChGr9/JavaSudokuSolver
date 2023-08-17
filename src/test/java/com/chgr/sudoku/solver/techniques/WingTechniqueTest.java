a8f3cf602305109ab7079890e1527b13e15816c5
Chris <cgregoriou9@gmail.com>
2023-08-17 10:28:17 +0300

    Implement jellyfish technique

--- src/test/java/com/chgr/sudoku/solver/techniques/WingTechniqueTest.java
+++ src/test/java/com/chgr/sudoku/solver/techniques/WingTechniqueTest.java
@@ -1,9 +1,11 @@
 package com.chgr.sudoku.solver.techniques;
 
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.io.IOException;
 import java.net.URL;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -32,7 +34,7 @@ class WingTechniqueTest extends BaseTechniqueTest {
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
-    void swordFish(int fileNumber) throws Exception {
+    void swordfish(int fileNumber) throws IOException {
         URL url = WingTechniqueTest.class.getResource(String.format("/techniques/swordfish%d.yml", fileNumber));
         assertNotNull(url);
 
@@ -48,6 +50,45 @@ class WingTechniqueTest extends BaseTechniqueTest {
         }
         assertChecksMatch(sudoku, technique.checks);
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4})
+    void jellyfish(int fileNumber) throws IOException {
+        URL url = WingTechniqueTest.class.getResource(String.format("/techniques/jellyfish%d.yml", fileNumber));
+        assertNotNull(url);
+
+        TechniqueEntity technique = mapper.readValue(url, TechniqueEntity.class);
+        assertNotNull(technique);
+        assertNotNull(technique.grid);
+
+        loadSudoku(technique);
+
+        for(int i=0; i < technique.repetitions; i++) {
+            boolean result = WingTechnique.jellyfish(sudoku);
+            assertTrue(result);
+        }
+        assertChecksMatch(sudoku, technique.checks);
+    }
+
+    @RepeatedTest(100)
+    void jellyfishtest() throws IOException {
+        int fileNumber = 4;
+        URL url = WingTechniqueTest.class.getResource(String.format("/techniques/jellyfish%d.yml", fileNumber));
+        assertNotNull(url);
+
+        TechniqueEntity technique = mapper.readValue(url, TechniqueEntity.class);
+        assertNotNull(technique);
+        assertNotNull(technique.grid);
+
+        loadSudoku(technique);
+
+        for(int i=0; i < technique.repetitions; i++) {
+            boolean result = WingTechnique.jellyfish(sudoku);
+            assertTrue(result);
+        }
+        assertChecksMatch(sudoku, technique.checks);
+    }
+
 
     @Test
     void yWing() throws Exception {
